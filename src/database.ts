d2720ff58a29ed64836519e075965d7cefd47728
Bernardo Campos <bernardocjs@gmail.com>
2023-07-05 21:12:44 -0300

    feat: :card_file_box: adds first database migration

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,15 @@
-import  {knex, Knex}  from 'knex'
+import knex, { Knex } from "knex";
 
 export const config: Knex.Config = {
-        client: 'sqlite3',
-        connection: {
-            filename: './db/app.db'
-        },
-        useNullAsDefault: true,
-        migrations: {
-            extension: 'ts',
-            directory: './db/migrations'
-        }
-}
+  client: "sqlite3",
+  connection: {
+    filename: "./db/app.db",
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations",
+  },
+};
 
-export const setupKnex  = knex(config)
+export const setupKnex = knex(config);
