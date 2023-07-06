6edba82f276954357d675756840d112e6e544db3
Bernardo Campos <bernardocjs@gmail.com>
2023-07-05 22:09:49 -0300

    build: :heavy_plus_sign: adds zod dependency

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { setupKnex } from "./database";
 import crypto from "node:crypto";
+import { env } from "./env";
 
 const server = fastify();
 
@@ -18,7 +19,7 @@ server.get("/hello", async () => {
 
 server
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log("HTTP server running");
